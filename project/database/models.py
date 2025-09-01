2655cb5d99b3d5856eca6fc7abe4107e26d5776f
Clinton Odia <odiamehiclinton@gmail.com>
2025-09-01 15:39:19 +0100

    feat(model) : implement user db model

---
+++ project/database/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy.sql.sqltypes import String, Integer
+from sqlalchemy import Column
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
